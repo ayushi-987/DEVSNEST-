904e216eff290334cf710d720a0a1565ea3ed204
Ayushi Soni <ayushisoni987@gmail.com>
2021-09-19 16:29:59 +0530

    Day-5 THA completed

---
+++ index.js
@@ -0,0 +1,7 @@
+// var fs = require("fs");
+// const Test = require("./require_test");
+// fs.mkdirSync("day-5.2");
+var Test =require("./require_test");
+
+var obj = new Test();
+console.log(obj.print());
---
+++ require_test.js
@@ -0,0 +1,6 @@
+class Test{
+    print(){
+        console.log("I am inside Require test");
+    }
+}
+module.exports = Test;
